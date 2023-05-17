23d9ef9cbcc990e60763c46f87b698d63fb3eddf
Luke Fromhold <luke@fromhold.io>
2023-05-17 16:39:42 +1000

    Overhaul with compat for SS 4 & 5, plus configuration docs

--- README.md
+++ README.md
@@ -1,3 +1,84 @@
 # silverstripe-configured-groups
 
 Silverstripe module that provides mechanism to configure security groups via yml config.
+
+## Requirements
+
+- 2.x branch currently supports both SilverStripe 4 & 5.
+
+## Rationale
+
+For many sites, the core security groups and their key permissions should not be changed by CMS users. 
+
+It leaves open the possibility of users breaking permission configurations, deletion of groups that would have consequential impacts throughout the site, and so forth.
+
+Having these managed via CMS also means that developers moving & deploying between environments must port group configurations manually.
+
+Instead, this module provides yml config:
+
+- Configuration of groups, and their assigned permissions, initialised on dev/build
+- If no ADMIN group is configured, the default 'administrators' group will remain in place
+- Can force only configured groups as root groups (ie. CMS user cannot add new top-level groups)
+- On per configured group basis, dis/allow CMS users adding new sub-groups (by extension, allowing or preventing CMS users adding new groups at all)
+- Can force only configured permissions per group (ie. any permissions set via CMS checkbox, that are not in yml config, are deleted on dev/build)
+- For configured groups, title, description and parent cms fields are read-only
+- Where a configured group's permissions are set to only a configured set, the permissions checkbox set is read-only
+- A configured group can be marked as hidden, and will not be displayed to users who are not direct members of that group (used to obfuscate the administrators group when only utilised by developers, for instance)
+
+## Installation
+
+Install the module using composer:
+
+```
+composer require fromholdio/silverstripe-configured-groups dev-master
+```
+
+Then apply your own group configuration (see below) and run dev/build.
+
+## Config examples
+
+- Out of the box, no configuration is applied, only the extension itself. You need to compose your groups configuration per example below: 
+
+```yml
+SilverStripe\Security\Group:
+  is_configured_root_groups_only: true      # only configured groups as top-level (cms users cannot add new with parentID 0)
+  is_configured_permissions_only: true      # only configured permissions on this group (others deleted during dev/build)
+  configured_groups:
+    administrators:                         # configured group code
+      title: Administrators
+      sort: 0
+      is_hidden: true                       # hides group from view of non-direct-members
+      permissions:                          # array of permission codes
+        - ADMIN
+    managers:     
+      title: Site managers
+      description: 'Primary non-developer super-user account type'
+      sort: 1
+      is_configured_children_only: true     # only configured groups as sub-groups (cms users cannot add new groups with parent ID as this group's ID)
+      permissions:
+        - CMS_ACCESS_CMSMain
+        - CMS_ACCESS_AssetAdmin
+        - CMS_ACCESS_ReportAdmin
+        - CMS_ACCESS_SecurityAdmin
+        - SITETREE_REORGANISE
+        - EDIT_SITECONFIG
+    previewers:
+      title: Previewers
+      sort: 2
+      children:
+        previewer-special:
+          title: 'Special previewers'
+          sort: 0
+          permissions:
+            - CMS_ACCESS_CMSMain
+            - VIEW_DRAFT_CONTENT
+        previewer-okay:
+          title: 'Just okay previewers'
+          sort: 1
+          permissions:
+            - VIEW_DRAFT_CONTENT
+```
+
+## License
+
+BSD 3-Clause License, see [License](LICENSE)
--- src/ConfiguredGroups.php
+++ src/ConfiguredGroups.php
@@ -2,132 +2,98 @@
 
 namespace Fromholdio\ConfiguredGroups;
 
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\ValidationResult;
 use SilverStripe\Security\Group;
 use SilverStripe\Security\Permission;
+use SilverStripe\Security\Security;
 
 class ConfiguredGroups extends DataExtension
 {
     private static $configured_groups;
 
-    private static $do_clear_unconfigured_permissions = false;
+    private static $is_configured_permissions_only = false;
+    private static $is_configured_root_groups_only = false;
+
 
     public function requireDefaultRecords()
     {
-        $config = $this->getOwner()->config()->get('configured_groups');
-        if (!empty($config) && is_array($config))
+        $config = $this->getOwner()->getConfiguredGroupsConfig();
+        if (empty($config)) return;
+        $this->doConfigureGroups($config);
+    }
+
+
+    public function doConfigureGroups(array $config): void
+    {
+        if (empty($config)) return;
+
+        foreach ($config as $code => $data) {
+            $this->getOwner()->doConfigureGroup($code, $data);
+        }
+
+        // Delete not-configured root-level groups if only configured groups allowed
+        if ($this->getOwner()->isConfiguredRootGroupsOnly())
         {
-            foreach ($config as $code => $data) {
-                $this->getOwner()->createConfiguredGroup($code, $data);
-            }
-
-            if (!isset($data['content-authors']))
-            {
-                $defaultGroup = Group::get()->find('Code', 'content-authors');
-                if ($defaultGroup && $defaultGroup->exists()) {
-                    $defaultGroup->delete();
-                }
-            }
-
-            if (!isset($data['administrators']))
-            {
-                $configuredAdminCodes = $this->getOwner()->getAllConfiguredGroupAdminCodes();
-                $adminGroups = Permission::get_groups_by_permission('ADMIN')->exclude('Code', 'administrators');
-                if (!empty($configuredAdminCodes) && $adminGroups->count() > 0)
-                {
-                    $defaultAdminGroup = Group::get()->find('Code', 'administrators');
-                    if ($defaultAdminGroup && $defaultAdminGroup->exists()) {
-                        $defaultAdminGroup->delete();
-                    }
+            $rootGroups = Group::get()
+                ->filter('ParentID', 0)
+                ->exclude('Code', 'administrators');
+            foreach ($rootGroups as $rootGroup) {
+                if (!$rootGroup->isConfiguredGroup()) {
+                    $rootGroup->delete();
                 }
             }
         }
-    }
 
-    public function getAllConfiguredGroupAdminCodes(): array
-    {
-        $codes = [];
-        $config = $this->getOwner()->config()->get('configured_groups');
-        if (!empty($config) && is_array($config))
+        // Delete default administrators group,
+        // if other group has been configured with ADMIN permission
+        if (!$this->getOwner()->isConfiguredGroup('administrators'))
         {
-            foreach ($config as $code => $data)
+            $configuredAdminCodes = $this->getOwner()->getAllConfiguredAdminGroupCodes();
+            $adminGroups = Permission::get_groups_by_permission('ADMIN')->exclude('Code', 'administrators');
+            if (!empty($configuredAdminCodes) && $adminGroups->count() > 0)
             {
-                $groupAdminCodes = $this->getOwner()->getConfiguredGroupAdminCodes($code, $data, true);
-                if (!empty($groupAdminCodes)) {
-                    $codes += $groupAdminCodes;
+                $defaultAdminGroup = Group::get()->find('Code', 'administrators');
+                if ($defaultAdminGroup?->exists()) {
+                    $defaultAdminGroup->delete();
                 }
             }
         }
-        return $codes;
     }
 
-    public function getConfiguredGroupAdminCodes(string $code, array $data, bool $checkChildren = false): array
+    public function doConfigureGroup(string $code, array $config, int $parentGroupID = 0): void
     {
-        $codes = [];
-        if ($this->getOwner()->isConfiguredGroupAdmin($data)) {
-            $codes[] = $code;
-        }
-
-        $children = $data['children'] ?? null;
-        if (!empty($children) && is_array($children)) {
-            foreach ($children as $childCode => $childData) {
-                $childAdminCodes = $this->getOwner()->getConfiguredGroupAdminCodes($childCode, $childData, $checkChildren);
-                if (!empty($childAdminCodes)) {
-                    $codes += $childAdminCodes;
-                }
-            }
-        }
-        return $codes;
-    }
-
-    public function isConfiguredGroupAdmin(array $data, bool $checkChildren = false): bool
-    {
-        $isAdmin = false;
-
-        $permissions = $data['permissions'] ?? null;
-        if (!empty($permissions) && is_array($permissions)) {
-            $isAdmin = in_array('ADMIN', $permissions);
-        }
-
-        if (!$isAdmin && $checkChildren)
-        {
-            $children = $data['children'] ?? null;
-            if (!empty($children) && is_array($children)) {
-                foreach ($children as $childData) {
-                    $isAdmin = $this->getOwner()->isConfiguredGroupAdmin($childData, $checkChildren);
-                    if ($isAdmin) break;
-                }
-            }
-        }
-
-        return $isAdmin;
-    }
-
-    public function createConfiguredGroup(string $code, array $data, int $parentGroupID = 0): void
-    {
-        $title = $data['title'] ?? $code;
-        $sort = $data['sort'] ?? null;
-        $permissions = $data['permissions'] ?? null;
-        $children = $data['children'] ?? null;
+        $title = $config['title'] ?? $code;
+        $description = $config['description'] ?? null;
+        $sort = $config['sort'] ?? null;
+        $permissions = $config['permissions'] ?? null;
+        $children = $config['children'] ?? null;
+        $isConfigChildrenOnly = $config['is_configured_children_only'] ?? false;
 
         $group = Group::get()->find('Code', $code);
         if (!$group || !$group->exists())
         {
             $group = Group::create();
-            $group->Code = $code;
+            $group->setField('Code', $code);
         }
 
-        $group->Title = $title;
-        $group->Sort = $sort;
-        $group->ParentID = $parentGroupID;
+        $group->setField('Title', $title);
+        $group->setField('Description', $description);
+        $group->setField('Sort', $sort);
+        $group->setField('ParentID', $parentGroupID);
         $group->write();
+        $groupID = (int) $group->getField('ID');
 
         if (!empty($permissions) && is_array($permissions))
         {
-            if ($this->getOwner()->config()->get('do_clear_unconfigured_permissions'))
+            if ($this->getOwner()->isConfiguredPermissionsOnly())
             {
                 $unconfigPermissions = Permission::get()
-                    ->filter('GroupID', $group->getField('ID'))
+                    ->filter('GroupID', $groupID)
                     ->exclude('Code', array_values($permissions));
                 foreach ($unconfigPermissions as $unconfigPermission) {
                     $unconfigPermission->delete();
@@ -136,21 +102,310 @@ class ConfiguredGroups extends DataExtension
             foreach ($permissions as $permission)
             {
                 $existingPermission = Permission::get()->filter([
-                    'GroupID' => $group->getField('ID'),
+                    'GroupID' => $groupID,
                     'Code' => $permission
                 ])->first();
-                if (!$existingPermission || !$existingPermission->exists()) {
-                    Permission::grant($group->ID, $permission);
+                if (!$existingPermission?->exists()) {
+                    Permission::grant($groupID, $permission);
                 }
             }
         }
 
         if (!empty($children) && is_array($children))
         {
-            $groupID = (int) $group->ID;
             foreach ($children as $childCode => $childData) {
-                $this->getOwner()->createConfiguredGroup($childCode, $childData, $groupID);
+                $this->getOwner()->doConfigureGroup($childCode, $childData, $groupID);
+            }
+            if ($isConfigChildrenOnly) {
+                $childGroups = Group::get()->filter('ParentID', $groupID);
+                foreach ($childGroups as $childGroup) {
+                    if (!$childGroup->isConfiguredGroup()) {
+                        $childGroup->delete();
+                    }
+                }
             }
         }
+    }
+
+
+    public function getConfiguredGroupsConfig(): array
+    {
+        $config = $this->getOwner()->config()->get('configured_groups');
+        $this->getOwner()->invokeWithExtensions('updateConfiguredGroupsConfig', $config);
+        return empty($config) || !is_array($config) ? [] : $config;
+    }
+
+    public function getConfiguredGroupConfig(string $code): ?array
+    {
+        $config = $this->getOwner()->getAllConfiguredGroupsConfig();
+        if (empty($config)) return null;
+        $codeConfig = $config[$code] ?? null;
+        return is_array($codeConfig) ? $codeConfig : null;
+    }
+
+    public function getConfiguredGroupConfigValue(string $key, ?string $code = null): array|string|bool|null
+    {
+        if (empty($code)) {
+            $code = $this->getOwner()->getField('Code');
+        }
+        if (empty($code)) {
+            return null;
+        }
+        $config = $this->getOwner()->getConfiguredGroupConfig($code);
+        return $config[$key] ?? null;
+    }
+
+
+    public function getAllConfiguredGroupsConfig(?array $config = null): array
+    {
+        $allConfig = [];
+        if (is_null($config)) {
+            $config = $this->getOwner()->getConfiguredGroupsConfig();
+            if (empty($config)) return $allConfig;
+        }
+        foreach ($config as $key => $data)
+        {
+            $allConfig[$key] = $data;
+            $childrenConfig = $data['children'] ?? null;
+            if (!empty($childrenConfig)) {
+                $allChildrenConfig = $this->getOwner()->getAllConfiguredGroupsConfig($childrenConfig);
+                if (!empty($allChildrenConfig)) {
+                    $allConfig = [...$allConfig, ...$allChildrenConfig];
+                }
+            }
+        }
+        return $allConfig;
+    }
+
+    public function getAllConfiguredGroupCodes(): array
+    {
+        $config = $this->getOwner()->getAllConfiguredGroupsConfig();
+        return empty($config) ? [] : array_keys($config);
+    }
+
+    public function getAllConfiguredAdminGroupCodes(): array
+    {
+        $adminCodes = [];
+        $config = $this->getOwner()->getAllConfiguredGroupCodes();
+        foreach ($config as $code) {
+            if ($this->getOwner()->isConfiguredAdminGroup('ADMIN', $code)) {
+                $adminCodes[] = $code;
+            }
+        }
+        return $adminCodes;
+    }
+
+    /**
+     * Return codes of configured groups that allow configured & non-configured (added new by cms user) groups as children.
+     * @return array
+     */
+    public function getAllConfiguredGroupCodesAllowNewChildren(): array
+    {
+        $codes = [];
+        $allCodes = $this->getOwner()->getAllConfiguredGroupCodes();
+        foreach ($allCodes as $code) {
+            if (!$this->getOwner()->isConfiguredChildGroupsOnly($code)) {
+                $codes[] = $code;
+            }
+        }
+        return $codes;
+    }
+
+    /**
+     * Return codes of configured groups that allow only configured groups as children.
+     * @return array
+     */
+    public function getAllConfiguredGroupCodesDisallowNewChildren(): array
+    {
+        $codes = [];
+        $allCodes = $this->getOwner()->getAllConfiguredGroupCodes();
+        foreach ($allCodes as $code) {
+            if ($this->getOwner()->isConfiguredChildGroupsOnly($code)) {
+                $codes[] = $code;
+            }
+        }
+        return $codes;
+    }
+
+
+    public function isConfiguredGroupsOnly(): bool
+    {
+        return $this->getOwner()->isConfiguredRootGroupsOnly()
+            && empty($this->getOwner()->getAllConfiguredGroupCodesAllowNewChildren());
+    }
+
+    public function isConfiguredRootGroupsOnly(): bool
+    {
+        return (bool) $this->getOwner()->config()->get('is_configured_root_groups_only');
+    }
+
+    public function isConfiguredPermissionsOnly(): bool
+    {
+        return (bool) $this->getOwner()->config()->get('is_configured_permissions_only');
+    }
+
+
+    public function isConfiguredGroup(?string $code = null): bool
+    {
+        if (empty($code)) {
+            $code = $this->getOwner()->getField('Code');
+        }
+        if (empty($code)) {
+            return false;
+        }
+        $config = $this->getOwner()->getConfiguredGroupConfig($code);
+        return !empty($config);
+    }
+
+    public function isConfiguredAdminGroup(?string $code = null): bool
+    {
+        return $this->getOwner()->isConfiguredGroupPermitted('ADMIN', $code);
+    }
+
+    public function isConfiguredGroupPermitted(string $permissionCode, ?string $code = null): bool
+    {
+        if (empty($code)) {
+            $code = $this->getOwner()->getField('Code');
+        }
+        if (!$this->getOwner()->isConfiguredGroup($code)) {
+            return false;
+        }
+        $permissions = $this->getOwner()->getConfiguredGroupConfigValue('permissions', $code);
+        if (!is_array($permissions)) {
+            return false;
+        }
+        return in_array($permissionCode, $permissions);
+    }
+
+    public function isConfiguredChildGroupsOnly(?string $code = null): bool
+    {
+        if (empty($code)) {
+            $code = $this->getOwner()->getField('Code');
+        }
+        if (!$this->getOwner()->isConfiguredGroup($code)) {
+            return false;
+        }
+        return (bool) $this->getOwner()->getConfiguredGroupConfigValue('is_configured_children_only', $code);
+    }
+
+
+    public function validate(ValidationResult $validationResult): void
+    {
+        if (!$this->getOwner()->isConfiguredGroup())
+        {
+            if ($this->getOwner()->isConfiguredGroupsOnly()) {
+                $validationResult->addError('Groups must be managed via configuration only.');
+            }
+            else {
+                $parent = $this->getOwner()->Parent();
+                if ($parent?->exists()) {
+                    if ($parent->isConfiguredChildGroupsOnly()) {
+                        $validationResult->addError('You must select a valid parent group.');
+                    }
+                }
+                elseif ($this->getOwner()->isConfiguredRootGroupsOnly()) {
+                    $validationResult->addError('You must select a valid parent group.');
+                }
+            }
+        }
+    }
+
+
+    public function getConfiguredDecodedBreadcrumbs(): ArrayList
+    {
+        $list = ArrayList::create();
+        if (!$this->getOwner()->isConfiguredGroupsOnly())
+        {
+            $groups = Group::get()->exclude('ID', $this->getOwner()->getField('ID'));
+            $disallowNewChildCodes = $this->getOwner()->getAllConfiguredGroupCodesDisallowNewChildren();
+            if (!empty($disallowNewChildCodes)) {
+                $groups = $groups->exclude('Code', $disallowNewChildCodes);
+            }
+            $member = Security::getCurrentUser();
+            foreach ($groups as $group) {
+                if ($group->canView($member)) {
+                    $list->push([
+                        'ID' => $group->getField('ID'),
+                        'Title' => $group->getBreadcrumbs(' » ')
+                    ]);
+                }
+            }
+        }
+        return $list;
+    }
+
+    public function updateCMSFields(FieldList $fields): void
+    {
+        if ($this->getOwner()->isConfiguredGroup()) {
+            $fields->dataFieldByName('Title')->setReadonly(true);
+            if (empty($this->getOwner()->getField('Description'))) {
+                $fields->removeByName('Description');
+            }
+            else {
+                $fields->dataFieldByName('Description')?->setReadonly(true);
+            }
+            if ($this->getOwner()->Parent()?->exists()) {
+                $parentIDField = ReadonlyField::create(
+                    'ParentBreadcrumbs',
+                    $this->getOwner()->fieldLabel('Parent'),
+                    $this->getOwner()->Parent()->getBreadcrumbs(' » ')
+                );
+                $fields->replaceField('ParentID', $parentIDField);
+            }
+            else {
+                $fields->removeByName('ParentID');
+            }
+            if ($this->getOwner()->isConfiguredPermissionsOnly()) {
+                $fields->dataFieldByName('Permissions')?->setReadonly(true);
+            }
+            return;
+        }
+
+        /** @var DropdownField $parentIDField */
+        $parentIDField = $fields->dataFieldByName('ParentID');
+        if (!$parentIDField) return;
+
+        $configuredParentsList = $this->getConfiguredDecodedBreadcrumbs();
+        if ($configuredParentsList->count() > 0) {
+            $parentIDField->setSource($configuredParentsList);
+            if ($this->getOwner()->isConfiguredRootGroupsOnly()) {
+                $parentIDField->setHasEmptyDefault(false);
+            }
+        }
+        else {
+            $fields->removeByName('ParentID');
+        }
+    }
+
+
+    public function canCreate($member): bool
+    {
+        if ($this->getOwner()->isConfiguredGroupsOnly()) {
+            return false;
+        }
+        return Permission::checkMember($member, 'CMS_ACCESS_SecurityAdmin');
+    }
+
+    public function canView($member): ?bool
+    {
+        if ($this->getOwner()->isConfiguredGroup()) {
+            $isHidden = (bool) $this->getOwner()->getConfiguredGroupConfigValue('is_hidden');
+            if ($isHidden) {
+                $canView = false;
+                if ($member?->exists()) {
+                    $canView = $member->inGroup($this->getOwner(), true);
+                }
+                return $canView;
+            }
+        }
+        return null;
+    }
+
+    public function canDelete($member): ?bool
+    {
+        if ($this->getOwner()->isConfiguredGroup()) {
+            return false;
+        }
+        return null;
     }
 }
